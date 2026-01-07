56b2bcfe9d47b90a9ce7755d13a0d5c7860d8842
Guilherme Nunes <guilherme.augd@gmail.com>
2026-01-07 11:42:23 -0300

    set up component for screen size responsiveness

---
+++ components/main-layout.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+
+interface MainLayoutProps {
+  children: React.ReactNode;
+}
+
+export function MainLayout({ children}: MainLayoutProps) {
+  return(
+    <div className = "flex min-h-screen w-full flex-col items-center justify-center p-4 sm:p-8">
+      <div className="w-full max-w-md md:max-w-2xl">
+        {children}
+      </div>
+    </div>
+
+  );
+}
